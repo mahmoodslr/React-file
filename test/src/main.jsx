fa600675df0006d9faa37ab6533e2ad41bbf2b98
Mahmood <mahmoodsalari2005@gmail.com>
2025-02-10 19:49:34 +0330

    Added min-React

---
+++ test/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+<<<<<<< HEAD
+import App from "./App";
+=======
+import App from "./App2";
+>>>>>>> c345fb2 (Added min-React)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
