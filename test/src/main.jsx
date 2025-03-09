ad1082ee95e06bddf03baa5d9524534c0152824a
Mahmood salari <mahmoodsalari2005@gmail.com>
2025-03-09 20:33:20 +0330

    Update main.jsx

--- test/src/main.jsx
+++ test/src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-<<<<<<< HEAD
 import App from "./App";
-=======
-import App from "./App2";
->>>>>>> c345fb2 (Added min-React)
+// import App from "./App2";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
